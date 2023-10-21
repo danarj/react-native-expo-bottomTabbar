143a2950ed9e239da1b4ef6524f03b8b8ded49ed
danarj <danarj@live.com>
2023-10-21 17:02:34 +0300

    work

--- src/screens/Products.js
+++ src/screens/Products.js
@@ -1,10 +1,15 @@
 import { StyleSheet, Text, View } from "react-native";
 import React from "react";
+import { Image } from "expo-image";
 
 const Products = () => {
   return (
     <View style={styles.container}>
       <Text>Products</Text>
+      <Image
+        style={{ width: 32, height: 32 }}
+        source={require("../../assets/test.svg")}
+      />
     </View>
   );
 };
